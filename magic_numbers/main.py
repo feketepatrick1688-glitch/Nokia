ff27f6f82ce2095816ac60ff1302e0c131597daa
Fekete Patrik <feketepatrick1688@gmail.com>
2026-04-18 14:27:36 +0200

    1. Task

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,9 +1,19 @@
 from pathlib import Path
 
+def is_palindrome(n: int) -> bool:
+    s = str(n)
+    return s == s[::-1]
 
-def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+def next_magic_num(num: int) -> int:
+    if num < 0:
+        return 0
+    candidate = num + 1
+    while not is_palindrome(candidate):
+        candidate += 1
+    return candidate
+print("next_magic_num(808)  =>", next_magic_num(808))
+print("next_magic_num(999)  =>", next_magic_num(999))   
+print("next_magic_num(2133) =>", next_magic_num(2133)) 
 
 
 if __name__ == "__main__":
