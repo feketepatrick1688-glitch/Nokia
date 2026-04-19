8ca84f6cf6ac733e3982022fcf6451189e93b45c
Fekete Patrik <feketepatrick1688@gmail.com>
2026-04-19 15:43:28 +0200

    2. Task Rework

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,25 +1,21 @@
-from pathlib import Path
-
 def min_num_of_drops(n, h):
     dp = [0] * (n + 1)
     moves = 0
 
     while dp[n] < h:
         moves += 1
-        for i in range(n, 0, -1):
-            dp[i] = dp[i] + dp[i - 1] + 1
+        for k in range(n, 0, -1):
+            dp[k] = dp[k] + dp[k - 1] + 1
 
     return moves
 
 
 def main():
-    lines = Path("input.txt").read_text().splitlines()
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
 
     for line in lines:
-        if not line.strip():
-            continue
-
-        n, h = map(int, line.split(","))
+        n, h = map(int, line.strip().split(","))
         print(min_num_of_drops(n, h))
 
 
